342f324e2000a080d811b67497bda31f79870323
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-04-11 10:39:02 +0200

    switching to dynamic import

--- apps/app-simple/src/index.ts
+++ apps/app-simple/src/index.ts
@@ -1,1 +1,4 @@
-import "./bootstrap"
+// https://webpack.js.org/concepts/module-federation/#uncaught-error-shared-module-is-not-available-for-eager-consumption
+// says that with module-federation it's worth to put "an asynchronous boundary" by switching to import("./bootstrap")
+// import "./bootstrap"
+import("./bootstrap")
